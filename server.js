632733878d9cb9b684b2eae5f971528508faa4a9
BigPhatNerd <wilsonhorrell@gmail.com>
2024-04-13 17:33:25 -0500

    Added path

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const path = require("path");
 const connectDB = require("./config/dbConnection");
 const axios = require("axios");
 const app = express();
