91fc3e6442b726546c459f5630b00c292793ac3e
BigPhatNerd <wilsonhorrell@gmail.com>
2024-04-13 18:12:12 -0500

    build

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ connectDB();
 
 app.use(express.json());
 app.use("/api", routes);
-
+console.log("__dirname: ", __dirname);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
