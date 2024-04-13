ffe20020051abd34faa1cdaecf70c13395b9e731
BigPhatNerd <wilsonhorrell@gmail.com>
2024-04-13 18:02:52 -0500

    build issues

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,8 +8,8 @@ export default defineConfig({
     proxy: {
       "/api": "https://lhrlslacktest.ngrok.io",
     },
-     build: {
-    outDir: 'build', // Optional: Configure output directory (default is 'dist')
-  }
+    build: {
+      outDir: "./build", // Optional: Configure output directory (default is 'dist')
+    },
   },
 });
